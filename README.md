b0a48771971215ccc9b89c2b966c062123a416ee
cosho <rizeupbass@gmail.com>
2016-02-28 13:24:47 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# vscode-svgviewer
+SVG Viewer for Visual Studio Code
+[Visual Studio Marketplace](https://marketplace.visualstudio.com/items/cssho.vscode-svgviewer)
+
+## Usage 
+0. コマンドパレットを表示し、入力欄に"ext install"と入力してEnter
+0. "teratail Client"を選択し、インストール
+0. SVGファイルを開きコマンドパレットかショートカットから処理を選択
+
+![palette](img/palette.png)
+
+### View SVG - `Ctrl(Cmd)+I O`
+SVGをエディタ上で表示
+
+### Export PNG - `Ctrl(Cmd)+I E`
+SVGをPNGに変換
+
+![preview](img/preview.png)
---
+++ src/extension.ts
@@ -0,0 +1,105 @@
+'use strict';
+
+import * as vscode from 'vscode';
+
+const fs = require('pn/fs');
+const svg2png = require('svg2png');
+const tmp = require('tmp');
+
+
+export function activate(context: vscode.ExtensionContext) {
+
+    console.log('SVG Viewer is now active!');
+
+    let previewUri = vscode.Uri.parse('svg-preview://authority/svg-preview');
+
+    class SvgDocumentContentProvider implements vscode.TextDocumentContentProvider {
+        private _onDidChange = new vscode.EventEmitter<vscode.Uri>();
+
+        public provideTextDocumentContent(uri: vscode.Uri): string {
+            return this.createSvgSnippet();
+        }
+
+        get onDidChange(): vscode.Event<vscode.Uri> {
+            return this._onDidChange.event;
+        }
+
+        public update(uri: vscode.Uri) {
+            this._onDidChange.fire(uri);
+        }
+
+        private createSvgSnippet() {
+
+            return this.extractSnippet();
+        }
+
+        private extractSnippet(): string {
+            let editor = vscode.window.activeTextEditor;
+            let text = editor.document.getText();
+            return this.snippet(text);
+        }
+
+        private errorSnippet(error: string): string {
+            return `
+                <body>
+                    ${error}
+                </body>`;
+        }
+
+        private snippet(properties): string {
+            return `<!DOCTYPE html><html><body><div class="svgbg">${properties}</div></body></html>`;
+        }
+    }
+
+    let provider = new SvgDocumentContentProvider();
+    let registration = vscode.workspace.registerTextDocumentContentProvider('svg-preview', provider);
+
+    vscode.workspace.onDidChangeTextDocument((e: vscode.TextDocumentChangeEvent) => {
+        if (e.document === vscode.window.activeTextEditor.document) {
+            provider.update(previewUri);
+        }
+    });
+
+    vscode.window.onDidChangeTextEditorSelection((e: vscode.TextEditorSelectionChangeEvent) => {
+        if (e.textEditor === vscode.window.activeTextEditor) {
+            provider.update(previewUri);
+        }
+    })
+
+    let open = vscode.commands.registerTextEditorCommand('svgviewer.open', (te, t) => {
+        if (checkNoSvg(te)) return;
+        return vscode.commands.executeCommand('vscode.previewHtml', previewUri, vscode.ViewColumn.Two)
+            .then(s => console.log('done.'), vscode.window.showErrorMessage);
+
+    });
+
+    context.subscriptions.push(open);
+
+    let saveas = vscode.commands.registerTextEditorCommand('svgviewer.saveas', (te, t) => {
+        if (checkNoSvg(te)) return;
+        let editor = vscode.window.activeTextEditor;
+        let text = editor.document.getText();
+        let tmpobj = tmp.fileSync();
+        let pngpath = editor.document.uri.fsPath.replace('.svg', '.png');
+        fs.writeFile(tmpobj.name, text, 'utf-8')
+            .then(fs.readFile(tmpobj.name)
+                .then(svg2png)
+                .then(buffer => fs.writeFile(pngpath, buffer))
+                .catch(e => console.error(e))
+                .then(tmpobj.removeCallback())
+                .then(vscode.window.showInformationMessage('export done. ' + pngpath)));
+    });
+
+    context.subscriptions.push(saveas);
+}
+function checkNoSvg(editor :vscode.TextEditor) {
+    
+    let isNGType = !(editor.document.languageId === 'xml') || editor.document.getText().indexOf('</svg>') < 0;
+    if (isNGType) {
+        vscode.window.showWarningMessage("Active editor doesn't show a SVG document - no properties to preview.");
+    }
+    return isNGType;
+}
+
+export function deactivate() {
+}
